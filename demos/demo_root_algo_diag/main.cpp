71f1a39426f09125921d50e0b5dc15a98f1d9c75
AmyRouillard <rouillardamy@gmail.com>
2015-04-28 23:09:53 +0200

    code corrected

--- demos/demo_root_algo_diag/main.cpp
+++ demos/demo_root_algo_diag/main.cpp
@@ -1,3 +1,4 @@
+
 //Amy
 //Solving A*sigma=b in ROOT
 
@@ -17,67 +18,73 @@ using namespace std;
 Double_t func_source_sheet(Double_t * coord, Double_t * par)
 {
 	Double_t s = coord[0];
-	
-	//par[0]=r inradius
-	//par[1]=R outer radius
+
+	//par[0]=x_c
+	//par[1]y_c
 	//par[2]=beta_j
 	//par[3]=beta_i
-	
-	Double_t x_c=par[1]*cos(par[3]);
-	Double_t y_c=par[1]*sin(par[3]);
-	
-	Double_t x_a=par[0]*cos(par[2]-TMath::Pi()/8);
-	Double_t y_a=par[0]*sin(par[2]-TMath::Pi()/8);
-	
-	Double_t a=(x_c-(x_a-s*sin(par[2])))*cos(par[3])+(y_c-(y_a+s*cos(par[2])))*sin(par[3]);
-	Double_t b=pow(x_c-(x_a-s*sin(par[2])),2)+pow(y_c-(y_a+s*cos(par[2])),2);
-		
-	return a/b;
+	//par[4]=x_a
+	//par[5]=y_a
+
+	Double_t x_c = par[0];
+	Double_t y_c = par[1];
+
+	Double_t x_a = par[4];
+	Double_t y_a = par[5];
+
+	Double_t a = (x_c - (x_a - s * sin(par[2]))) * cos(par[3]) + (y_c - (y_a + s * cos(par[2]))) * sin(par[3]);
+	Double_t b = pow(x_c - (x_a - s * sin(par[2])), 2) + pow(y_c - (y_a + s * cos(par[2])), 2);
+
+	return a / b;
 }
+
 
 Double_t func_vortex_sheet(Double_t * coord, Double_t * par)
 {
 	Double_t s = coord[0];
-	
-	Double_t x_c=par[1]*cos(par[3]);
-	Double_t y_c=par[1]*sin(par[3]);
-	
-	Double_t x_a=par[0]*cos(par[2]-TMath::Pi()/8);
-	Double_t y_a=par[0]*sin(par[2]-TMath::Pi()/8);
-	
-	//par[0]=r inradius
-	//par[1]=R outer radius
+
+	//par[0]=x_c
+	//par[1]y_c
 	//par[2]=beta_j
 	//par[3]=beta_i
-	
-	Double_t a=sin(par[3])*(x_c-(x_a-s*sin(par[2])))-cos(par[3])*(y_c-(y_a+s*cos(par[2])));
-	Double_t b=pow(x_c-(x_a-s*sin(par[2])),2)+pow(y_c-(y_a+s*cos(par[2])),2);
-	
-	return a/b;
+	//par[4]=x_a
+	//par[5]=y_a
+
+	Double_t x_c = par[0];
+	Double_t y_c = par[1];
+	Double_t x_a = par[4];
+	Double_t y_a = par[5];
+
+	Double_t a = sin(par[3]) * (x_c - (x_a - s * sin(par[2]))) - cos(par[3]) * (y_c - (y_a + s * cos(par[2])));
+	Double_t b = pow(x_c - (x_a - s * sin(par[2])), 2) + pow(y_c - (y_a + s * cos(par[2])), 2);
+
+	return a / b;
 }
 
 Double_t func_vortex_sheet_2(Double_t * coord, Double_t * par)
 {
 	Double_t s = coord[0];
-	
-	Double_t x_c=par[1]*cos(par[3]);
-	Double_t y_c=par[1]*sin(par[3]);
-	
-	Double_t x_a=par[0]*cos(par[2]-TMath::Pi()/8);
-	Double_t y_a=par[0]*sin(par[2]-TMath::Pi()/8);
-	
-	//par[0]=r inradius
-	//par[1]=R outer radius
+
+	//par[0]=x_c
+	//par[1]y_c
 	//par[2]=beta_j
 	//par[3]=beta_i
-	
-	Double_t a=cos(par[3])*(x_c-(x_a-s*sin(par[2])))-sin(par[3])*(y_c-(y_a+s*cos(par[2])));
-	Double_t b=pow(x_c-(x_a-s*sin(par[2])),2)+pow(y_c-(y_a+s*cos(par[2])),2);
-	
-	return a/b;
+	//par[4]=x_a
+	//par[5]=y_a
+
+	Double_t x_c = par[0];
+	Double_t y_c = par[1];
+	Double_t x_a = par[4];
+	Double_t y_a = par[5];
+
+	Double_t a = cos(par[3]) * (x_c - (x_a - s * sin(par[2]))) - sin(par[3]) * (y_c - (y_a + s * cos(par[2])));
+	Double_t b = pow(x_c - (x_a - s * sin(par[2])), 2) + pow(y_c - (y_a + s * cos(par[2])), 2);
+
+	return a / b;
 }
 
-int main(){
+int main()
+{
 	/*
 	//Simple example:
 	Double_t n=2;
@@ -96,141 +103,235 @@ int main(){
 	b(0)=0;
 	b(1)=6;
 	*/
-	
+
 	/////////////////////////////////////////////////
 	//Octagon:
 
-	Double_t n=8; //octagon
+	Double_t n = 8; //octagon
 
-	TVectorD b(n+1);
-	TMatrixD A(n+1,n+1);
+	//TVectorD b(n);
+	//TMatrixD A(n,n);
 
-	Double_t gamma=1; //vortex strength
-	Double_t U_inf=1;
-	Double_t alpha=0; //Angle of attack
-	Double_t R=1; //outer radius 
-	Double_t r=R*(1+sqrt(2))/sqrt(4+2*sqrt(2)); //inradius (octagon)
-	
-	Double_t s_0=0;
-	Double_t s_1=R*2/sqrt(4+2*sqrt(2));
-	
-	TF1 Z("Z",func_source_sheet,s_0,s_1,4);
-	Z.SetParameter(0,R);
-	Z.SetParameter(1,r);
+	TVectorD b(n + 1);
+	TMatrixD A(n + 1, n + 1);
+
+
+	Double_t U_inf = 1;
+	Double_t alpha = 0; //Angle of attack
+
+	///////////////////////////////
+
+	TVectorD x_c(n);
+	TVectorD y_c(n);
+	TVectorD x_a(n);
+	TVectorD y_a(n);
+
+	x_c(0) = 4;
+	x_c(1) = 3;
+	x_c(2) = 0;
+	x_c(3) = -3;
+	x_c(4) = -4;
+	x_c(5) = -3;
+	x_c(6) = 0;
+	x_c(7) = 3;
+
+	y_c(0) = 0;
+	y_c(1) = 3;
+	y_c(2) = 4;
+	y_c(3) = 3;
+	y_c(4) = 0;
+	y_c(5) = -3;
+	y_c(6) = -4;
+	y_c(7) = -3;
+
+	x_a(0) = 4;
+	x_a(1) = 4;
+	x_a(2) = 2;
+	x_a(3) = -2;
+	x_a(4) = -4;
+	x_a(5) = -4;
+	x_a(6) = -2;
+	x_a(7) = 2;
+
+	y_a(0) = -2;
+	y_a(1) = 2;
+	y_a(2) = 4;
+	y_a(3) = 4;
+	y_a(4) = 2;
+	y_a(5) = -2;
+	y_a(6) = -4;
+	y_a(7) = -4;
+
+	Double_t s_0 = 0;
+	TVectorD s_1(n);
+
+	s_1(0) = 4;
+	s_1(1) = 2 * sqrt(2);
+	s_1(2) = 4;
+	s_1(3) = 2 * sqrt(2);
+	s_1(4) = 4;
+	s_1(5) = 2 * sqrt(2);
+	s_1(6) = 4;
+	s_1(7) = 2 * sqrt(2);
+
+	////////////////////////////////
+
+
 
 	ROOT::Math::GaussIntegrator GI_Z;
 	GI_Z.SetRelTolerance(0.001);
-	
-		
-	for (int i= 0; i < n; i++) {
-		b(i)= -U_inf*cos(TMath::Pi()/4*i)*cos(alpha);
-		ROOT::Math::WrappedTF1 wrap_Z(Z);
-		GI_Z.SetFunction(wrap_Z);
-		
-		for (int j= 0; j < n; j++) {
-			if (i==j) 
-			{A(i,j)=0.5;}
-			else {
-				Z.SetParameter(2, TMath::Pi()/4*j);
-				Z.SetParameter(3, TMath::Pi()/4*i);
-				
-				A(i,j)=0.5/TMath::Pi()*GI_Z.Integral(s_0,s_1);
-				}
+
+
+	for(int i = 0; i < n; i++)
+	{
+		b(i) = -U_inf * cos(TMath::Pi() / 4 * i) * cos(alpha);
+
+		for(int j = 0; j < n; j++)
+		{
+			if(i == j)
+			{
+				A(i, j) = 0.5;
+			}
+			else
+			{
+
+				//par[0]=x_c
+				//par[1]y_c
+				//par[2]=beta_j
+				//par[3]=beta_i
+				//par[4]=x_a
+				//par[5]=y_a
+				TF1 Z("Z", func_source_sheet, s_0, s_1(j), 6);
+				Z.SetParameter(0, x_c(i));
+				Z.SetParameter(1, y_c(i));
+				Z.SetParameter(2, TMath::Pi() / 4 * j);
+				Z.SetParameter(3, TMath::Pi() / 4 * i);
+				Z.SetParameter(4, x_a(j));
+				Z.SetParameter(5, y_a(j));
+
+				ROOT::Math::WrappedTF1 wrap_Z(Z);
+				GI_Z.SetFunction(wrap_Z);
+
+				A(i, j) = 1. / 2. / TMath::Pi() * GI_Z.Integral(s_0, s_1(j));
+			}
 		}
 	}
-	
-	//VORTEX:
-	
-	Double_t beta_l=TMath::Pi();
-	Double_t beta_k=0;
-	
-	TF1 ZZ("ZZ",func_vortex_sheet,s_0,s_1,4);
-	ZZ.SetParameter(0,R);
-	ZZ.SetParameter(1,r);
-	
+
+
+	//VORTEX: %%%FIX!!%%%
+
+	Double_t gamma = 1; //vortex strength
+	Double_t beta_l = TMath::Pi();
+	Double_t beta_k = 0;
+
+
+
 	ROOT::Math::GaussIntegrator GI_ZZ;
 	GI_ZZ.SetRelTolerance(0.001);
-	
-	for (int i= 0; i < n; i++) {
-		
-		int j=n;
-				
+
+	for(int i = 0; i < n; i++)
+	{
+
+		int j = n - 1;
+		TF1 ZZ("ZZ", func_vortex_sheet, s_0, s_1(j), 6);
+
+		ZZ.SetParameter(0, x_c(i));
+		ZZ.SetParameter(1, y_c(i));
+		ZZ.SetParameter(2, TMath::Pi() / 4 * j);
+		ZZ.SetParameter(3, TMath::Pi() / 4 * i);
+		ZZ.SetParameter(4, x_a(j));
+		ZZ.SetParameter(5, y_a(j));
+
 		ROOT::Math::WrappedTF1 wrap_ZZ(ZZ);
 		GI_ZZ.SetFunction(wrap_ZZ);
-		
-		ZZ.SetParameter(2, TMath::Pi()/4*j);
-		ZZ.SetParameter(3, TMath::Pi()/4*i);
-		
-		A(i,j)= -0.5*gamma/TMath::Pi()*GI_ZZ.Integral(s_0,s_1);
+		A(i, j + 1) = -0.5 * gamma / TMath::Pi() * GI_ZZ.Integral(s_0, s_1(j));
 	}
-		
-	for (int j= 0; j < n; j++) {
-		
-		int i=n;
-		
-		b(i)= -U_inf*cos(alpha-beta_k)-U_inf*cos(alpha-beta_l);
-		
-		ROOT::Math::WrappedTF1 wrap_ZZ(ZZ);
-		GI_ZZ.SetFunction(wrap_ZZ);
-		
-		ZZ.SetParameter(3, TMath::Pi()/4*i);
-		
-		
+
+	for(int j = 0; j < n; j++)
+	{
+
+		int i = n - 1;
+
+		b(i) = -U_inf * cos(alpha - beta_k) - U_inf * cos(alpha - beta_l);
+
+		TF1 ZZ("ZZ", func_vortex_sheet, s_0, s_1(j), 6);
+
+		ZZ.SetParameter(0, x_c(i));
+		ZZ.SetParameter(1, y_c(i));
+		ZZ.SetParameter(3, TMath::Pi() / 4 * i);
+		ZZ.SetParameter(4, x_a(j));
+		ZZ.SetParameter(5, y_a(j));
+
 		ZZ.SetParameter(2, beta_l);
-		A(i,j)= -0.5*gamma/TMath::Pi()*GI_ZZ.Integral(s_0,s_1);
-		
+		ROOT::Math::WrappedTF1 wrap_ZZ(ZZ);
+		GI_ZZ.SetFunction(wrap_ZZ);
+		A(i, j) = -0.5 * gamma / TMath::Pi() * GI_ZZ.Integral(s_0, s_1(j));
+
 		ZZ.SetParameter(2, beta_k);
-		A(i,j)= A(i,j)-0.5*gamma/TMath::Pi()*GI_ZZ.Integral(s_0,s_1);
-		
+		ROOT::Math::WrappedTF1 wrap_ZZ_2(ZZ);
+		GI_ZZ.SetFunction(wrap_ZZ_2);
+		A(i, j + 1) = A(i, j + 1) - 0.5 * gamma / TMath::Pi() * GI_ZZ.Integral(s_0, s_1(j));
+
 	}
-	
-	
-	TF1 ZZZ("ZZZ",func_vortex_sheet_2,s_0,s_1,4);
-	ZZZ.SetParameter(0,R);
-	ZZZ.SetParameter(1,r);
-	
-	int i=n;
-	int j=n;
-	
+
+
+
+
+	int i = n - 1;
+	int j = n - 1;
+
 	ROOT::Math::GaussIntegrator GI_ZZZ;
 	GI_ZZZ.SetRelTolerance(0.001);
-	
-	ROOT::Math::WrappedTF1 wrap_ZZZ(ZZ);
-	GI_ZZZ.SetFunction(wrap_ZZZ);
-	
-	ZZZ.SetParameter(2, TMath::Pi()/4*j);
-	ZZZ.SetParameter(3, TMath::Pi()/4*i);
-	
+
+
+	TF1 ZZZ("ZZZ", func_vortex_sheet_2, s_0, s_1(j), 6);
+	ZZZ.SetParameter(0, x_c(i));
+	ZZZ.SetParameter(1, y_c(i));
+	ZZZ.SetParameter(3, TMath::Pi() / 4 * i);
+	ZZZ.SetParameter(4, x_a(j));
+	ZZZ.SetParameter(5, y_a(j));
+
 	ZZZ.SetParameter(2, beta_l);
-	A(i,j)= -0.5*gamma/TMath::Pi()*GI_ZZZ.Integral(s_0,s_1);
-	
+	ROOT::Math::WrappedTF1 wrap_ZZZ(ZZZ);
+	GI_ZZZ.SetFunction(wrap_ZZZ);
+	A(i, j + 1) = -0.5 * gamma / TMath::Pi() * GI_ZZZ.Integral(s_0, s_1(j));
+
 	ZZZ.SetParameter(2, beta_k);
-	A(i,j)= A(i,j)-0.5*gamma/TMath::Pi()*GI_ZZZ.Integral(s_0,s_1);
-		
+	ROOT::Math::WrappedTF1 wrap_ZZZ_2(ZZZ);
+	GI_ZZZ.SetFunction(wrap_ZZZ_2);
+	A(i, j + 1) = A(i, j + 1) - 0.5 * gamma / TMath::Pi() * GI_ZZZ.Integral(s_0, s_1(j));
+
 
 	///////////////////////////////////////////////////
 
 	TDecompLU lu(A);
 	Bool_t ok;
-	TVectorD x=lu.Solve(b,ok);
-	TVectorD ans=A*x;
-	ans=b-ans;
-	
-	if (x.IsValid()){
-		//A.Print();
-		//b.Print();
+	TVectorD x = lu.Solve(b, ok);
+	TVectorD ans = A * x;
+	ans = b - ans;
+
+	if(x.IsValid())
+	{
+		A.Print();
+		b.Print();
 		//lu.Print();
 		x.Print();
-		
+
 		cout << "solved with tol = " << lu.GetTol() << endl;
-		TVectorD r=b-A*x;
-		cout << "residue = " << lu.Condition()*r.NormInf()/A.NormInf()/x.NormInf() << endl; //?cond(A)=cond(lu)
+		TVectorD r = b - A * x;
+		cout << "residue = " << lu.Condition()*r.NormInf() / A.NormInf() / x.NormInf() << endl; //?cond(A)=cond(lu)
 		cout << "cond = " << lu.Condition() << endl;
-		cout << "b-A*x = "  << endl; ans.Print() ;
-		
-		}
+		cout << "b-A*x = "  << endl;
+		ans.Print() ;
+
+	}
 	else
-		{cout << "solving failed " << endl;}
+	{
+		cout << "solving failed " << endl;
+	}
 
 	return 0;
 }
+
+
+
--- demos/demo_root_algo_integral/main.cpp
+++ demos/demo_root_algo_integral/main.cpp
@@ -25,8 +25,7 @@ Double_t func_dvx(Double_t * coord, Double_t * par)
 //Velocity v_x analytical solution
 Double_t func_vx(Double_t ksi_min, Double_t ksi_max)
 {
-	//return sigma * x / 2 / TMath::Pi() * TMath::Log((x * x + (y - ksi_min)) / (x * x + (y - ksi_max)));
-	return -sigma/2/ TMath::Pi()*(atan2(y-ksi_max,x)-atan2(y-ksi_min,x));
+	return -sigma / 2 / TMath::Pi() * (atan2(y - ksi_max, x) - atan2(y - ksi_min, x));
 }
 
 //Velocity dv_y
@@ -39,8 +38,7 @@ Double_t func_dvy(Double_t * coord, Double_t * par)
 //Velocity v_y analytical solution
 Double_t func_vy(Double_t ksi_min, Double_t ksi_max)
 {
-	//return sigma / TMath::Pi() * TMath::Log((x * x + (y - ksi_min)*(y - ksi_min)) / (x * x + (y - ksi_max)*(y - ksi_max)));
-	return sigma /4/TMath::Pi() * TMath::Log((x * x + (y - ksi_min)*(y - ksi_min)) / (x * x + (y - ksi_max)*(y - ksi_max)));
+	return sigma / 4 / TMath::Pi() * TMath::Log((x * x + (y - ksi_min) * (y - ksi_min)) / (x * x + (y - ksi_max) * (y - ksi_max)));
 }
 
 
@@ -48,14 +46,14 @@ Double_t func_vy(Double_t ksi_min, Double_t ksi_max)
 Double_t func_A(Double_t * coord, Double_t * par)
 {
 	Double_t s = coord[0];
-	Double_t x_c=par[1]*cos(par[2]);
-	Double_t y_c=par[1]*sin(par[2]);
-	Double_t x_a=par[0]*cos(par[2]-TMath::Pi()/8);
-	Double_t y_a=par[0]*sin(par[2]-TMath::Pi()/8);
-	
-	Double_t a=(x_c-(x_a-s*sin(par[2])))*cos(par[2])+(y_c-(y_a-s*cos(par[2])))*sin(par[2]);
-	Double_t b=pow(x_c-(x_a-s*sin(par[2])),2)+pow(y_c-(y_a-s*cos(par[2])),2);
-	return a/b;
+	Double_t x_c = par[1] * cos(par[3]);
+	Double_t y_c = par[1] * sin(par[3]);
+	Double_t x_a = par[0] * cos(par[2] - TMath::Pi() / 8);
+	Double_t y_a = par[0] * sin(par[2] - TMath::Pi() / 8);
+
+	Double_t a = (x_c - (x_a - s * sin(par[2]))) * cos(par[3]) + (y_c - (y_a - s * cos(par[2]))) * sin(par[3]);
+	Double_t b = pow(x_c - (x_a - s * sin(par[2])), 2) + pow(y_c - (y_a - s * cos(par[2])), 2);
+	return a / b;
 }
 
 int main()
@@ -68,26 +66,31 @@ int main()
 	vx.SetParameter(1, y);
 	vx.SetParameter(2, sigma);
 	Double_t I_vx;
-	
+
 	TF1 vy("vy", func_dvy, a, b, 3);
 	vy.SetParameter(0, x);
 	vy.SetParameter(1, y);
 	vy.SetParameter(2, sigma);
 	Double_t I_vy;
-	
+
 	ROOT::Math::WrappedTF1 wrap_vx(vx);
 	ROOT::Math::WrappedTF1 wrap_vy(vy);
 
 	//printf("exact result vx\t= % .7f\n", func_vx(a, b));
 	//printf("exact result vy\t= % .7f\n", func_vy(a, b));
-	
+
 	//Methode: GaussIntegrator
 	ROOT::Math::GaussIntegrator GI;
 	GI.SetRelTolerance(0.001);
-	
+
 	GI.SetFunction(wrap_vx);
 	start = clock();
-	for(int i = 0; i < 1000; i++) I_vx= GI.Integral(a,b);
+
+	for(int i = 0; i < 1000; i++)
+	{
+		I_vx = GI.Integral(a, b);
+	}
+
 	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	printf("Methode:GaussIntegrator, ");
 	printf("vx:\n");
@@ -97,22 +100,32 @@ int main()
 
 	GI.SetFunction(wrap_vy);
 	start = clock();
-	for(int i = 0; i < 1000; i++) I_vy= GI.Integral(a,b);
+
+	for(int i = 0; i < 1000; i++)
+	{
+		I_vy = GI.Integral(a, b);
+	}
+
 	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	printf("Methode:GaussIntegrator, ");
 	printf("vy:\n");
 	printf("result \t\t= % .10f\n", I_vy);
 	printf("error \t\t= % .10f\n", I_vy - func_vy(a, b));
 	printf("duration \t= % .10f\n", duration);
-	
+
 	//Methode: GaussLegendreIntegrator
 	ROOT::Math::GaussLegendreIntegrator GLI;
 	GLI.SetRelTolerance(0.001);
 	GLI.SetNumberPoints(1000);
-	
+
 	GLI.SetFunction(wrap_vx);
 	start = clock();
-	for(int i = 0; i < 1000; i++) I_vx = GLI.Integral(a, b);
+
+	for(int i = 0; i < 1000; i++)
+	{
+		I_vx = GLI.Integral(a, b);
+	}
+
 	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	printf("Methode:GaussLegendreIntegrator, ");
 	printf("vx:\n");
@@ -122,7 +135,12 @@ int main()
 
 	GLI.SetFunction(wrap_vy);
 	start = clock();
-	for(int i = 0; i < 1000; i++) I_vy = GLI.Integral(a, b);
+
+	for(int i = 0; i < 1000; i++)
+	{
+		I_vy = GLI.Integral(a, b);
+	}
+
 	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	printf("Methode:GaussLegendreIntegrator, ");
 	printf("vy:\n");
@@ -132,7 +150,12 @@ int main()
 
 	//Methode: GSL
 	start = clock();
-	for(int i = 0; i < 1000; i++) I_vx = vx.Integral(a, b);
+
+	for(int i = 0; i < 1000; i++)
+	{
+		I_vx = vx.Integral(a, b);
+	}
+
 	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	printf("Methode:GSL, ");
 	printf("vx:\n");
@@ -141,49 +164,56 @@ int main()
 	printf("duration \t= % .10f\n", duration);
 
 	start = clock();
-	for(int i = 0; i < 1000; i++) I_vy = vy.Integral(a, b);
+
+	for(int i = 0; i < 1000; i++)
+	{
+		I_vy = vy.Integral(a, b);
+	}
+
 	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	printf("Methode:GSL, ");
 	printf("vy:\n");
 	printf("result \t\t= % .10f\n", I_vy);
 	printf("error \t\t= % .10f\n", I_vy - func_vy(a, b));
 	printf("duration \t= % .10f\n", duration);
-	
-	
+
+
 	/////////////////////////////////////////////////////
-	Double_t i=1;
-	Double_t R=1; //radius
-	Double_t r=R*(1+sqrt(2))/sqrt(4+2*sqrt(2)); //inradius
-	Double_t s_0=0;
-	Double_t s_1=R*2/sqrt(4+2*sqrt(2));
-	
-	TF1 Z("Z", func_A, s_0,s_1, 3);
+	Double_t i = 1;
+	Double_t j = 1;
+	Double_t R = 1; //radius
+	Double_t r = R * (1 + sqrt(2)) / sqrt(4 + 2 * sqrt(2)); //inradius
+	Double_t s_0 = 0;
+	Double_t s_1 = R * 2 / sqrt(4 + 2 * sqrt(2));
+
+	TF1 Z("Z", func_A, s_0, s_1, 4);
 	Z.SetParameter(0, R);
 	Z.SetParameter(1, r);
-	Z.SetParameter(2, TMath::Pi()/4*i);
-	
+	Z.SetParameter(3, TMath::Pi() / 4 * i);
+	Z.SetParameter(2, TMath::Pi() / 4 * j);
+
 	ROOT::Math::WrappedTF1 wrap_Z(Z);
-	
+
 	//Methode: GaussIntegrator
 	GI.SetFunction(wrap_Z);
 	printf("Methode:GaussIntegrator\n");
-	printf("result \t\t= % .10f\n", GI.Integral(s_0,s_1));
+	printf("result \t\t= % .10f\n", GI.Integral(s_0, s_1));
 	//printf("error \t\t= % .10f\n", 0);
 
-	
+
 	//Methode: GaussLegendreIntegrator
 	GLI.SetFunction(wrap_Z);
 	printf("Methode:GaussLegendreIntegrator\n");
-	printf("result \t\t= % .10f\n", GLI.Integral(s_0,s_1));
+	printf("result \t\t= % .10f\n", GLI.Integral(s_0, s_1));
 	//printf("error \t\t= % .10f\n", 0);
 
 
 	//Methode: GSL
 	printf("Methode:GSL\n");
-	printf("result \t\t= % .10f\n", Z.Integral(s_0,s_1));
+	printf("result \t\t= % .10f\n", Z.Integral(s_0, s_1));
 	//printf("error \t\t= % .10f\n", 0);
 
-	
-	
+
+
 	return 0;
 }
