da5b511da1d72fe0efe1bf942661942377b291d3
Dolf Huybrechts <dolf.huybrechts@student.uantwerpen.be>
2015-04-28 14:37:14 +0200

    demo vtk dolf toegevoegd versie2

--- demos/demo_vtk_comp_dolf/main.cpp
+++ demos/demo_vtk_comp_dolf/main.cpp
@@ -30,89 +30,94 @@
 int main()
 {
 	std::cout << "Creating grid... " << std::endl;
-	float grens=1;
-	int bin=21;
+	float grens = 1;
+	int bin = 21;
 	// grid dimensions
 	static int dims[3] = { bin, bin , 1 };
 	int size = dims[0] * dims[1] * dims[2];
 
 	// vectors
 	vtkSmartPointer< vtkFloatArray > vectors = vtkSmartPointer< vtkFloatArray >::New();
-	vectors->SetNumberOfComponents( 3 );
-	vectors->SetNumberOfTuples( size );
+	vectors->SetNumberOfComponents(3);
+	vectors->SetNumberOfTuples(size);
 
 	// points
 	vtkSmartPointer< vtkPoints > points = vtkSmartPointer< vtkPoints >::New();
-	points->Allocate( size );
+	points->Allocate(size);
 
 	// fill in data
 	float x[3];
 	float v[3];
-	float stapgrootte=(2*grens)/bin;
+	float stapgrootte = (2 * grens) / bin;
 	std::cout << "x in [" << -grens << "," << grens << "]" << endl;
 	std::cout << "y in [" << -grens << "," << grens << "]" << endl;
 	std::cout << "stapgrootte: " << stapgrootte << endl;
-    for ( int j= 0; j<dims[1]; j++ ) {
-        float jOffset = j * dims[0];
-        for ( int i = 0; i < dims[0]; i++ ) {
-            x[0] = j*stapgrootte;
-            x[1] = i*stapgrootte;
-            v[0] = (x[0]-(dims[0]/2)*stapgrootte);
-            v[1] = (x[1]-(dims[1]/2)*stapgrootte);
+
+	for(int j = 0; j < dims[1]; j++)
+	{
+		float jOffset = j * dims[0];
+
+		for(int i = 0; i < dims[0]; i++)
+		{
+			x[0] = j * stapgrootte;
+			x[1] = i * stapgrootte;
+			v[0] = (x[0] - (dims[0] / 2) * stapgrootte);
+			v[1] = (x[1] - (dims[1] / 2) * stapgrootte);
 //            v[0] = (x[0]-(dims[0]/2)*stapgrootte)/sqrt(pow(x[0]-(dims[0]/2)*stapgrootte,2)+pow(x[1]-(dims[1]/2)*stapgrootte,2));
 //            v[1] = (x[1]-(dims[1]/2)*stapgrootte)/sqrt(pow(x[0]-(dims[0]/2)*stapgrootte,2)+pow(x[1]-(dims[1]/2)*stapgrootte,2));
-            float offset = i + jOffset;
-            points->InsertPoint ( offset , x );
-            vectors->InsertTuple ( offset , v );
+			float offset = i + jOffset;
+			points->InsertPoint(offset , x);
+			vectors->InsertTuple(offset , v);
 		}
-    }
+	}
+
 	// combine into structured grid data set
 	vtkSmartPointer< vtkPolyData> grid =
-	vtkSmartPointer< vtkPolyData>::New();
+	    vtkSmartPointer< vtkPolyData>::New();
 	//grid->SetDimensions( dims );
-	grid->SetPoints( points );
-	grid->GetPointData()->SetVectors( vectors );
+	grid->SetPoints(points);
+	grid->GetPointData()->SetVectors(vectors);
 
-  vtkSmartPointer<vtkArrowSource> arrowSource =
-    vtkSmartPointer<vtkArrowSource>::New();
+	vtkSmartPointer<vtkArrowSource> arrowSource =
+	    vtkSmartPointer<vtkArrowSource>::New();
 
-  vtkSmartPointer<vtkGlyph3D> glyph3D =
-    vtkSmartPointer<vtkGlyph3D>::New();
-  glyph3D->SetSourceConnection(arrowSource->GetOutputPort());
-  //glyph3D->SetVectorModeToUseVector();
+	vtkSmartPointer<vtkGlyph3D> glyph3D =
+	    vtkSmartPointer<vtkGlyph3D>::New();
+	glyph3D->SetSourceConnection(arrowSource->GetOutputPort());
+	//glyph3D->SetVectorModeToUseVector();
 #if VTK_MAJOR_VERSION <= 5
-  glyph3D->SetInput(grid);
+	glyph3D->SetInput(grid);
 #else
-  glyph3D->SetInputData(input);
+	glyph3D->SetInputData(input);
 #endif
 
 
 	glyph3D->SetScaleModeToScaleByVector();
-  	//glyph3D->OrientOff();
+	//glyph3D->OrientOff();
 	glyph3D->SetScaleFactor(.1);
 	glyph3D->Update();
 
-  // Visualize
-  vtkSmartPointer<vtkPolyDataMapper> mapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
-  mapper->SetInputConnection(glyph3D->GetOutputPort());
+	// Visualize
+	vtkSmartPointer<vtkPolyDataMapper> mapper =
+	    vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputConnection(glyph3D->GetOutputPort());
 
-  vtkSmartPointer<vtkActor> actor =
-    vtkSmartPointer<vtkActor>::New();
-  actor->SetMapper(mapper);
+	vtkSmartPointer<vtkActor> actor =
+	    vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
 
-  vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  renderWindowInteractor->SetRenderWindow(renderWindow);
+	vtkSmartPointer<vtkRenderer> renderer =
+	    vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderWindow> renderWindow =
+	    vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+	    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	renderWindowInteractor->SetRenderWindow(renderWindow);
 
-  renderer->AddActor(actor);
-  renderer->SetBackground(0, 0,0); // Background color green
+	renderer->AddActor(actor);
+	renderer->SetBackground(0, 0, 0); // Background color green
 
-  renderWindow->Render();
-  renderWindowInteractor->Start();
+	renderWindow->Render();
+	renderWindowInteractor->Start();
 }
