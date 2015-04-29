e5e41a1719adba3225ad4bdcbb4968ed521d9db1
Andy Martinez <am41119@hotmail.com>
2015-04-29 17:32:08 +0200

    Fixed style

--- demos/demo_algo_integrationspeed/IntegrationSpeedTest.cpp
+++ demos/demo_algo_integrationspeed/IntegrationSpeedTest.cpp
@@ -8,20 +8,22 @@
 
 
 
-double panelFunctionGSL(double s,  void* params){
-	double* myParams = (double*) params;
+double panelFunctionGSL(double s,  void * params)
+{
+	double * myParams = (double *) params;
 	double xCenteri = myParams[0];
 	double yCenteri = myParams[1];
 	double betaCurrenti = myParams[2];
 	double betaOtherj = myParams[3];
 	double xaOtherj = myParams[4];
 	double yaOtherj = myParams[5];
-	double panelF = ((xCenteri - (xaOtherj - s*sin(betaOtherj)))*cos(betaCurrenti) + (yCenteri - (yaOtherj + s*cos(betaOtherj)))*sin(betaCurrenti))/
-					(pow(xCenteri - (xaOtherj - s*sin(betaOtherj)),2)+pow(yCenteri - (yaOtherj + s*cos(betaOtherj)),2));
+	double panelF = ((xCenteri - (xaOtherj - s * sin(betaOtherj))) * cos(betaCurrenti) + (yCenteri - (yaOtherj + s * cos(betaOtherj))) * sin(betaCurrenti)) /
+	                (pow(xCenteri - (xaOtherj - s * sin(betaOtherj)), 2) + pow(yCenteri - (yaOtherj + s * cos(betaOtherj)), 2));
 	return panelF;
 }
 
-double panelFunctionROOT(double* s,  double* params){
+double panelFunctionROOT(double * s,  double * params)
+{
 	double ss = s[0];
 	double xCenteri = params[0];
 	double yCenteri = params[1];
@@ -29,8 +31,8 @@ double panelFunctionROOT(double* s,  double* params){
 	double betaOtherj = params[3];
 	double xaOtherj = params[4];
 	double yaOtherj = params[5];
-	double panelF = ((xCenteri - (xaOtherj - ss*sin(betaOtherj)))*cos(betaCurrenti) + (yCenteri - (yaOtherj + ss*cos(betaOtherj)))*sin(betaCurrenti))/
-					(pow(xCenteri - (xaOtherj - ss*sin(betaOtherj)),2)+pow(yCenteri - (yaOtherj + ss*cos(betaOtherj)),2));
+	double panelF = ((xCenteri - (xaOtherj - ss * sin(betaOtherj))) * cos(betaCurrenti) + (yCenteri - (yaOtherj + ss * cos(betaOtherj))) * sin(betaCurrenti)) /
+	                (pow(xCenteri - (xaOtherj - ss * sin(betaOtherj)), 2) + pow(yCenteri - (yaOtherj + ss * cos(betaOtherj)), 2));
 	return panelF;
 }
 
@@ -41,10 +43,10 @@ int main()
 	// VERZONNEN WAARDEN: 8 PANELS IN OCTAGON STRUCTUUR OP CIRKEL MET STRAAL 4
 	std::vector<double> xCenter = {4, 3, 0, -3, -4, -3, 0, 3};
 	std::vector<double> yCenter = {0, 3, 4, 3, 0, -3, -4, -3};
-	std::vector<double> beta = {0, pi/4, pi/2, 3*pi/4, pi, 5*pi/4, 3*pi/2, 7*pi/4};
+	std::vector<double> beta = {0, pi / 4, pi / 2, 3 * pi / 4, pi, 5 * pi / 4, 3 * pi / 2, 7 * pi / 4};
 	std::vector<double> xa = {4, 4, 2, -2, -4, -4, -2, 2};
-	std::vector<double> ya = {-2, 2, 4, 4, 2, -2, -4, -4};
-	std::vector<double> s = {4, 2*pow(2,0.5), 4, 2*pow(2,0.5), 4, 2*pow(2,0.5), 4, 2*pow(2,0.5)};
+	std::vector<double> ya = { -2, 2, 4, 4, 2, -2, -4, -4};
+	std::vector<double> s = {4, 2 * pow(2, 0.5), 4, 2 * pow(2, 0.5), 4, 2 * pow(2, 0.5), 4, 2 * pow(2, 0.5)};
 
 	int nParams = 6;	//AANTAL PARAMETERS IN DE TE INTEGREREN FUNCTIE (te integreren parameter niet meegeteld !)
 	double params[nParams];
@@ -79,69 +81,87 @@ int main()
 	integrationMethodGSL = "CQUAD";
 	start = std::clock();
 	std::cout << "Method: " << integrationMethodGSL << std::endl;
-	for (int i=0; i<nExecutions; i++){
+
+	for(int i = 0; i < nExecutions; i++)
+	{
 		integrationResult = myGSLIntegrator.integrate(a, b, integrationMethodGSL).result;
 	}
+
 	std::cout << "Result: " << integrationResult << std::endl;
-	std::cout << "Duration: " << (std::clock() - start)/(double)CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Duration: " << (std::clock() - start) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	std::cout << "\n";
 
 
 	integrationMethodGSL = "QAG";
 	start = std::clock();
 	std::cout << "Method: " << integrationMethodGSL << std::endl;
-	for (int i=0; i<nExecutions; i++){
+
+	for(int i = 0; i < nExecutions; i++)
+	{
 		integrationResult = myGSLIntegrator.integrate(a, b, integrationMethodGSL).result;
 	}
+
 	std::cout << "Result: " << integrationResult << std::endl;
-	std::cout << "Duration: " << (std::clock() - start)/(double)CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Duration: " << (std::clock() - start) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	std::cout << "\n";
 
 
 	integrationMethodGSL = "QAGS";
 	start = std::clock();
 	std::cout << "Method: " << integrationMethodGSL << std::endl;
-	for (int i=0; i<nExecutions; i++){
+
+	for(int i = 0; i < nExecutions; i++)
+	{
 		integrationResult = myGSLIntegrator.integrate(a, b, integrationMethodGSL).result;
 	}
+
 	std::cout << "Result: " << integrationResult << std::endl;
-	std::cout << "Duration: " << (std::clock() - start)/(double)CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Duration: " << (std::clock() - start) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	std::cout << "\n";
 
 
 	integrationMethodGSL = "GL";
 	start = std::clock();
 	std::cout << "Method: " << integrationMethodGSL << std::endl;
-	for (int i=0; i<nExecutions; i++){
+
+	for(int i = 0; i < nExecutions; i++)
+	{
 		integrationResult = myGSLIntegrator.integrate(a, b, integrationMethodGSL).result;
 	}
+
 	std::cout << "Result: " << integrationResult << std::endl;
-	std::cout << "Duration: " << (std::clock() - start)/(double)CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Duration: " << (std::clock() - start) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	std::cout << "\n";
 
-	std::cout<<"ROOT INTEGRATION METHODS: " << std::endl;
+	std::cout << "ROOT INTEGRATION METHODS: " << std::endl;
 	std::cout << "\n";
 
 	integrationMethodRoot = "GI";
 	start = std::clock();
 	std::cout << "Method: " << integrationMethodRoot << std::endl;
-	for (int i=0; i<nExecutions; i++){
+
+	for(int i = 0; i < nExecutions; i++)
+	{
 		integrationResult = myROOTIntegrator.integrate(a, b, integrationMethodRoot);
 	}
+
 	std::cout << "Result: " << integrationResult << std::endl;
-	std::cout << "Duration: " << (std::clock() - start)/(double)CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Duration: " << (std::clock() - start) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	std::cout << "\n";
 
 	integrationMethodRoot = "GLI";
 	start = std::clock();
 	std::cout << "Method: " << integrationMethodRoot << std::endl;
-	for (int i=0; i<nExecutions; i++){
+
+	for(int i = 0; i < nExecutions; i++)
+	{
 		integrationResult = myROOTIntegrator.integrate(a, b, integrationMethodRoot);
 	}
+
 	std::cout << "Result: " << integrationResult << std::endl;
-	std::cout << "Duration: " << (std::clock() - start)/(double)CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Duration: " << (std::clock() - start) / (double)CLOCKS_PER_SEC << "s" << std::endl;
 	std::cout << "\n";
 
 
-    return 0;
+	return 0;
 }
--- demos/demo_algo_integrationspeed/ROOTIntegrator.cpp
+++ demos/demo_algo_integrationspeed/ROOTIntegrator.cpp
@@ -5,34 +5,40 @@
 #include <Math/GaussLegendreIntegrator.h>
 #include <Math/WrappedTF1.h>
 
-ROOTIntegrator::ROOTIntegrator(double(*integrationFunctiony)(double*, double*), int nParams){
+ROOTIntegrator::ROOTIntegrator(double(*integrationFunctiony)(double *, double *), int nParams)
+{
 	integrationFunction = new TF1("f", integrationFunctiony, 0, 1, nParams);
 }
 
-void ROOTIntegrator::setParameters(double* params){
+void ROOTIntegrator::setParameters(double * params)
+{
 	integrationFunction->SetParameters(params);
 }
 
-double ROOTIntegrator::integrate(double a, double b, std::string method){
+double ROOTIntegrator::integrate(double a, double b, std::string method)
+{
 	std::string lowerMethod = method;
 	std::transform(lowerMethod.begin(), lowerMethod.end(), lowerMethod.begin(), ::tolower);
 
 	ROOT::Math::WrappedTF1 wrapFunc(*integrationFunction);
 
-	if (lowerMethod == "gi"){
+	if(lowerMethod == "gi")
+	{
 		ROOT::Math::GaussIntegrator GI;
 		GI.SetRelTolerance(error);
 		GI.SetFunction(wrapFunc);
 		return GI.Integral(a, b);
 	}
-	else if (lowerMethod == "gli"){
+	else if(lowerMethod == "gli")
+	{
 		ROOT::Math::GaussLegendreIntegrator GLI;
 		GLI.SetRelTolerance(error);
 		GLI.SetNumberPoints(nPoints);
 		GLI.SetFunction(wrapFunc);
-		return GLI.Integral(a,b);
+		return GLI.Integral(a, b);
 	}
-	else{
+	else
+	{
 		std::cerr << "Method not recognized" << std::endl;
 		return -1;
 	}
