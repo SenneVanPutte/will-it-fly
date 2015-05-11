0bfc81de50c5cc94cf484c25de76433b052d3ac5
Senne Van Putte <sennevanputte@hotmail.com>
2015-05-11 22:39:53 +0200

    updates uphalen

--- wif_viz/visualization_vtk.cpp
+++ wif_viz/visualization_vtk.cpp
@@ -728,5 +728,177 @@ vtkSmartPointer<vtkStructuredGrid> visualization_vtk_c::construct_velocity_grid(
 	return combine_grid(velocity_bins, points, field);
 }
 
+vtkSmartPointer<vtkPlaneSource> visualization_vtk_c::construct_phi_plane() const
+{
+	vtkSmartPointer<vtkPlaneSource> plane = vtkSmartPointer<vtkPlaneSource>::New();
+	
+	plane->SetXResolution(round_abs(phi_bins.x));
+    plane->SetYResolution(round_abs(phi_bins.y));
+    plane->SetOrigin(min_range.x, min_range.y, 0);
+    plane->SetPoint1(max_range.x, min_range.y, 0);
+    plane->SetPoint2(min_range.x, max_range.y, 0);
+    plane->Update();
+	vtkSmartPointer<vtkDoubleArray> field = construct_field(phi_bins, true);
+
+	for(int i = 0; i < points->GetNumberOfPoints(); i++)
+	{
+		double x[3];
+
+		plane->GetOutput()->GetPoints()->GetPoint(i, x);
+
+		const vector_2d_c pos(x[0], x[1]);
+
+		double t = flow->get_phi(pos);
+
+		//field->InsertNextTuple1(t);
+		field->InsertNextValue(t);
+	}
+    plane->GetOutput()->GetPointData()->SetScalars(field);
+    return plane;
+}
+
+vtkSmartPointer<vtkPlaneSource> visualization_vtk_c::construct_psi_plane() const
+{
+	vtkSmartPointer<vtkPlaneSource> plane = vtkSmartPointer<vtkPlaneSource>::New();
+	
+	plane->SetXResolution(round_abs(psi_bins.x));
+    plane->SetYResolution(round_abs(psi_bins.y));
+    plane->SetOrigin(min_range.x, min_range.y, 0);
+    plane->SetPoint1(max_range.x, min_range.y, 0);
+    plane->SetPoint2(min_range.x, max_range.y, 0);
+    plane->Update();
+	vtkSmartPointer<vtkDoubleArray> field = construct_field(psi_bins, true);
+
+	for(int i = 0; i < points->GetNumberOfPoints(); i++)
+	{
+		double x[3];
+
+		plane->GetOutput()->GetPoints()->GetPoint(i, x);
+
+		const vector_2d_c pos(x[0], x[1]);
+
+		double t = flow->get_psi(pos);
+
+		//field->InsertNextTuple1(t);
+		field->InsertNextValue(t);
+	}
+    plane->GetOutput()->GetPointData()->SetScalars(field);
+    return plane;
+}
+
+
+void visualization_vtk_c::contour_plot(vtkSmartPointer<vtkPlaneSource> plane, int ncont) const
+{
+	double scalarRange[2];
+  plane->GetOutput()->GetPointData()->GetScalars()->GetRange(scalarRange);
+
+  vtkSmartPointer<vtkAppendPolyData> appendFilledContours =
+    vtkSmartPointer<vtkAppendPolyData>::New();
+
+  int numberOfContours = ncont;
+
+  double delta =
+    (scalarRange[1] - scalarRange[0]) /
+    static_cast<double> (numberOfContours - 1);
+
+  // Keep the clippers alive
+  std::vector<vtkSmartPointer<vtkClipPolyData> > clippersLo;
+  std::vector<vtkSmartPointer<vtkClipPolyData> > clippersHi;
+
+  for (int i = 0; i < numberOfContours; i++)
+    {
+    double valueLo = scalarRange[0] + static_cast<double> (i) * delta;
+    double valueHi = scalarRange[0] + static_cast<double> (i + 1) * delta;
+    clippersLo.push_back(vtkSmartPointer<vtkClipPolyData>::New());
+    clippersLo[i]->SetValue(valueLo);
+    if (i == 0)
+      {
+      clippersLo[i]->SetInputConnection(plane->GetOutputPort());
+      }
+    else
+      {
+      clippersLo[i]->SetInputConnection(clippersHi[i - 1]->GetOutputPort(1));
+      }
+    clippersLo[i]->InsideOutOff();
+    clippersLo[i]->Update();
+
+    clippersHi.push_back(vtkSmartPointer<vtkClipPolyData>::New());
+    clippersHi[i]->SetValue(valueHi);
+    clippersHi[i]->SetInputConnection(clippersLo[i]->GetOutputPort());
+    clippersHi[i]->GenerateClippedOutputOn();
+    clippersHi[i]->InsideOutOn();
+    clippersHi[i]->Update();
+    if (clippersHi[i]->GetOutput()->GetNumberOfCells() == 0)
+      {
+      continue;
+      }
+
+    vtkSmartPointer<vtkFloatArray> cd =
+      vtkSmartPointer<vtkFloatArray>::New();
+    cd->SetNumberOfComponents(1);
+    cd->SetNumberOfTuples(clippersHi[i]->GetOutput()->GetNumberOfCells());
+    cd->FillComponent(0, valueLo);
+
+    clippersHi[i]->GetOutput()->GetCellData()->SetScalars(cd);
+    appendFilledContours->AddInputConnection(clippersHi[i]->GetOutputPort());
+    }
+
+  vtkSmartPointer<vtkCleanPolyData> filledContours =
+    vtkSmartPointer<vtkCleanPolyData>::New();
+  filledContours->SetInputConnection(appendFilledContours->GetOutputPort());
+
+  vtkSmartPointer<vtkLookupTable> lut =
+    vtkSmartPointer<vtkLookupTable>::New();
+  lut->SetNumberOfTableValues(numberOfContours + 1);
+  lut->Build();
+  vtkSmartPointer<vtkPolyDataMapper> contourMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  contourMapper->SetInputConnection(filledContours->GetOutputPort());
+  contourMapper->SetScalarRange(scalarRange[0], scalarRange[1]);
+  contourMapper->SetScalarModeToUseCellData();
+  contourMapper->SetLookupTable(lut);
+
+  vtkSmartPointer<vtkActor> contourActor =
+    vtkSmartPointer<vtkActor>::New();
+  contourActor->SetMapper(contourMapper);
+  contourActor->GetProperty()->SetInterpolationToFlat();
+
+  vtkSmartPointer<vtkContourFilter> contours =
+    vtkSmartPointer<vtkContourFilter>::New();
+  contours->SetInputConnection(filledContours->GetOutputPort());
+  contours->GenerateValues(numberOfContours, scalarRange[0], scalarRange[1]);
+
+  vtkSmartPointer<vtkPolyDataMapper> contourLineMapperer =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  contourLineMapperer->SetInputConnection(contours->GetOutputPort());
+  contourLineMapperer->SetScalarRange(scalarRange[0], scalarRange[1]);
+  contourLineMapperer->ScalarVisibilityOff();
+
+  vtkSmartPointer<vtkActor> contourLineActor =
+    vtkSmartPointer<vtkActor>::New();
+  contourLineActor->SetMapper(contourLineMapperer);
+  contourLineActor->GetProperty()->SetLineWidth(2);
+
+  // The usual renderer, render window and interactor
+  vtkSmartPointer<vtkRenderer> ren1 =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renWin =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  vtkSmartPointer<vtkRenderWindowInteractor>
+    iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+
+  ren1->SetBackground(.1, .2, .3);
+  renWin->AddRenderer(ren1);
+  iren->SetRenderWindow(renWin);
+
+  // Add the actors
+  ren1->AddActor(contourActor);
+  ren1->AddActor(contourLineActor);
+
+  // Begin interaction
+  renWin->Render();
+  iren->Start();
+}
+
 
 } // namespace wif_viz
