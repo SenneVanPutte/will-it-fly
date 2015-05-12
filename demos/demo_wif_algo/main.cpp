fecf7607c2e3a06954a539e0e55dfdc071d96683
Ivo Wingelaar <ivo.wingelaar@gmail.com>
2015-05-12 16:13:59 +0200

    Shizzle gefixt

--- demos/demo_wif_algo/main.cpp
+++ demos/demo_wif_algo/main.cpp
@@ -8,19 +8,20 @@ int main()
 {
 	double radius = 2;
 	unsigned int corners = 10;
-	wif_core::vector_2d_c midpoint(0,0);
+	wif_core::vector_2d_c midpoint(0, 0);
 	wif_core::airfoil_c myAirfoil(midpoint, radius, corners);
 	std::shared_ptr<wif_core::uniform_flow_c> myFlow = std::make_shared<wif_core::uniform_flow_c>();
 	bool Kuta = false;
-	
-	wif_algo::calculation_results_c calculate_flow2 = wif_algo::calculate_flow(myAirfoil,myFlow,Kuta);
-	
-	for(unsigned int i =0; i<corners; i++)
+
+	wif_algo::calculation_results_c calculate_flow2 = wif_algo::calculate_flow(myAirfoil, myFlow, Kuta);
+
+	for(unsigned int i = 0; i < corners; i++)
 	{
-		std::cout<<calculate_flow2.c_p[i]<<std::endl; 
+		std::cout << calculate_flow2.c_p[i] << std::endl;
 	}
-	std::cout<<std::endl;
-	std::cout<<calculate_flow2.c_l<<std::endl;
+
+	std::cout << std::endl;
+	std::cout << calculate_flow2.c_l << std::endl;
 
 
 	return 0;
--- wif_algo/wif_algo.cpp
+++ wif_algo/wif_algo.cpp
@@ -117,7 +117,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 	double v_t_i;
 
 	//Check if one uses Kutta condition or not
-	if (!Kutta)
+	if(!Kutta)
 	{
 
 		unsigned int num_rows = num_lines;
@@ -135,7 +135,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 		//Setting matrix A and vector B to solve the system
 		for(unsigned int i = 0; i < num_rows; i++)
 		{
-			vector_b_data[i] = -U_inf * cos(angles[i]-angle_attack);
+			vector_b_data[i] = -U_inf * cos(angles[i] - angle_attack);
 
 			for(unsigned int j = 0; j < num_columns; j++)
 			{
@@ -205,7 +205,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 
 			}
 
-			c_p[i] = 1-pow((-U_inf * sin(angles[i]) + v_t_i)/U_inf,2);
+			c_p[i] = 1 - pow((-U_inf * sin(angles[i]) + v_t_i) / U_inf, 2);
 		}
 
 		//calculating c_l
@@ -238,7 +238,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 		//Fistr set matrix a and vector b
 		for(unsigned int i = 0; i < num_rows - 1; i++)
 		{
-			vector_b_data[i] = -U_inf * cos(angles[i]-angle_attack);
+			vector_b_data[i] = -U_inf * cos(angles[i] - angle_attack);
 
 			for(unsigned int j = 0; j < num_columns - 1; j++)
 			{
@@ -280,7 +280,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 			gsl_integration_cquad(&VORTEX1, s_0, lengths[j], 0., 1e-7, w4, &result, &error, &nevals);
 			gsl_integration_cquad_workspace_free(w4);
 
-			gsl_matrix_set(&matrix_A_view.matrix, (size_t) i, (size_t) j, -0.5/ pi * result);
+			gsl_matrix_set(&matrix_A_view.matrix, (size_t) i, (size_t) j, -0.5 / pi * result);
 		}
 
 		//Set last collumn of matrix A and set vector B
@@ -298,7 +298,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 			gsl_integration_cquad(&VORTEX1, s_0, lengths[j], 0., 1e-7, w5, &result, &error, &nevals);
 			gsl_integration_cquad_workspace_free(w5);
 
-			double result_temp = -0.5 /pi * result;
+			double result_temp = -0.5 / pi * result;
 
 			parameters = {angles[l], angles[j], centers[l].x, centers[l].y, points_airfoil[j].x, points_airfoil[j].y};
 
@@ -361,7 +361,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 
 		gsl_linalg_LU_solve(&matrix_A_view.matrix, p, &vector_b_view.vector, x);
 
-		std::vector<double> Sigma(num_columns-1);
+		std::vector<double> Sigma(num_columns - 1);
 
 		for(unsigned int i = 0; i < num_columns - 1; i++)
 		{
@@ -398,7 +398,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 
 			}
 
-			c_p[i] = 1-pow((-U_inf * sin(angles[i]) + v_t_i)/U_inf,2);
+			c_p[i] = 1 - pow((-U_inf * sin(angles[i]) + v_t_i) / U_inf, 2);
 		}
 
 		//Calculate c_l
@@ -418,7 +418,7 @@ calculation_results_c calculate_flow(const wif_core::airfoil_c & myAirfoil, std:
 			}
 		}
 
-		c_l = ll * Gamma *2/ U_inf / (x_max - x_min);
+		c_l = ll * Gamma * 2 / U_inf / (x_max - x_min);
 
 		//Building return
 		accumulate_flow->add_flow(myFlow);
