9241707f875e928f4a220b78c39a6aa5646772c2
thorvalddox <thorvalddx94@gmail.com>
2015-05-14 19:22:01 +0200

    airfoil fix

--- wif_core/airfoil_c.cpp
+++ wif_core/airfoil_c.cpp
@@ -63,7 +63,7 @@ airfoil_c::airfoil_c(const std::string & filename)
 		}
 
 		unsigned int half_size = this->points.size() / 2;
-		std::reverse(this->points.begin() + half_size, this->points.end());
+		std::reverse(this->points.begin(), this->points.end() - half_size - 1);
 	}
 	else
 	{
@@ -242,7 +242,7 @@ airfoil_c airfoil_c::closed_intersect(double epsilon) const
 	}
 
 	vector_2d_c endpoint;
-	this->get_lines().front().get_intersection(this->get_lines().back(), endpoint);
+	this->get_lines().front().get_intersection(this->get_lines().back(), endpoint, 0);
 	std::vector<vector_2d_c> newpoints;
 	newpoints.push_back(endpoint);
 	newpoints.insert(newpoints.end(), this->points.begin(), this->points.end());
