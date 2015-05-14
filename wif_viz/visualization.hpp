85d8f368df6268d005f28ff1960072f78af7c188
Kristof Dockx <kristof.dockx@student.uantwerpen.be>
2015-05-14 21:33:07 +0200

    update

--- wif_viz/visualization.hpp
+++ wif_viz/visualization.hpp
@@ -69,7 +69,7 @@ public:
 
 	void set_streamline_resolution(uint32_t streamline_resolution);
 
-	virtual void plotVectors(std::vector<std::vector<double>>, std::vector<double>, std::vector<std::string>);
+	virtual void plotVectors(std::vector<std::vector<double>>, std::vector<double>, std::vector<std::string>,std::string,std::string,std::string,std::string);
 
 	//
 
