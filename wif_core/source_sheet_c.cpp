b39a66a08ba3a7f76805f7cbb719eb4a22db6569
jelseg <segers.jelle@hotmail.com>
2015-05-15 12:27:56 +0200

    op hoop van zege

--- wif_core/source_sheet_c.cpp
+++ wif_core/source_sheet_c.cpp
@@ -55,6 +55,7 @@ double source_sheet_c::get_psi(const vector_2d_c & pos) const
 
 double source_sheet_c::get_phi(const vector_2d_c & pos) const
 {
+/*
 	double a = line.begin.y - pos.y;
 	double b = line.end.y - line.begin.y;
 	double c = line.begin.x - pos.x;
@@ -71,6 +72,26 @@ double source_sheet_c::get_phi(const vector_2d_c & pos) const
 	        + (2 * a * d - 2 * c * b) * at1 - 2 * b * b - 2 * d * d
 	        - (a * b + c * d) * log(a * a + c * c) - (2 * a * d - 2 * b * c) * at2)
 	       * temp3;
+	       */
+
+    double ymin = 0;
+	double ymax = 0;
+
+	vector_2d_c transpos = line.get_transformed(pos, ymin, ymax);
+	double x = transpos.x;
+	double y = transpos.y;
+
+	double coef1=x*x+(ymax-y)*(ymax-y);
+	double coef2=x*x+(ymin-y)*(ymin-y);
+	double ln1=log(coef1);
+	double ln2=log(coef2);
+	double at1=atan((ymax-y)/x);
+	double at2=atan((ymin-y)/x);
+	double coef3=sigma/(4*M_PI);
+
+	return ((ymax-y)*(ln1-2)+2*x*at1-(ymin-y)*(ln2-2)-2*x*at2)*coef3;
+
+
 }
 
 
